dfe57b2c1c35d7e7f267a58bd5a95e0fe3c21567
Evelyn2017 <evelinx@foxmail.com>
2020-02-01 12:04:40 +0800

    add AST projet

---
+++ AST/AST/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  AST
+//
+//  Created by Evelyn on 2020/2/1.
+//  Copyright © 2020 Evelyn. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
