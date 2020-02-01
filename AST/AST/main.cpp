ffa60a84a369c1d01b3669bc4a99b575c78a6ddf
Evelyn2017 <evelinx@foxmail.com>
2020-02-01 13:25:19 +0800

    add Token class

--- AST/AST/main.cpp
+++ AST/AST/main.cpp
@@ -7,9 +7,61 @@
 //
 
 #include <iostream>
+using namespace std;
+
+string INTEGER = "INTEGER";
+string PLUS = "PLUS";
+string MINUS = "MINUS";
+string END = "END";
+
+class Token {
+public:
+    string type;
+    string value;
+    
+    Token();
+    Token(string, string);
+    
+    string to_string();
+    
+    void error();
+};
+
+Token:: Token(string type, string value) {
+    this->type = type;
+    this->value = value;
+}
+
+std:: ostream& operator<< (std:: ostream &strm, const Token &token) {
+    return strm << "Token <" << token.type << ", " << token.value << ">" << endl;
+}
+
+///////////////////////////////////////////////
+// Lexical analysis part.                    //
+///////////////////////////////////////////////
+class Tokenizer {
+public:
+    string text;
+    int pos;
+    char current_char = text[pos];
+};
+
+
+///////////////////////////////////////////////
+// Parser part.                              //
+///////////////////////////////////////////////
+class Parser {
+public:
+    Tokenizer tokenizer;
+    Token current_token;
+    
+    void error();
+    
+    
+};
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    Token t = Token("PLUS", "+");
+    cout << t <<endl;
     return 0;
 }
