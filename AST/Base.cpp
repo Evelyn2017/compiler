b2a05639a2ae753878e8b8e62407bda1d1942630
Evelyn2017 <evelinx@foxmail.com>
2020-02-04 22:38:10 +0800

    add tokenizer to return a token stream.

---
+++ AST/Base.cpp
@@ -0,0 +1,9 @@
+//
+//  Base.cpp
+//  AST
+//
+//  Created by Evelyn on 2020/2/2.
+//  Copyright © 2020 Evelyn. All rights reserved.
+//
+
+#include "Base.hpp"
---
+++ AST/Base.hpp
@@ -0,0 +1,14 @@
+//
+//  Base.hpp
+//  AST
+//
+//  Created by Evelyn on 2020/2/2.
+//  Copyright © 2020 Evelyn. All rights reserved.
+//
+
+#ifndef Base_hpp
+#define Base_hpp
+
+#include <stdio.h>
+
+#endif /* Base_hpp */
